05fe43b97abc5696f6c57c76230b58e6444f6f6e
Peggymailula <peggymailula@gmail.com>
2026-06-23 21:45:49 +0200

    Add transport Intelligence gif

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -4,6 +4,7 @@ import marketplace from "../../assets/video-to-gif-converter.gif"
 import lunarTour from "../../assets/LunarTour-ezgif.com-video-to-gif-converter.gif"
 import valourVoice from "../../assets/ValourVoice-ezgif.com-video-to-gif-converter.gif"
 import empath from "../../assets/assign-ezgif.com-video-to-gif-converter.gif"
+import graphs from "../../assets/graph-ezgif.com-video-to-gif-converter.gif"
 
 import ScrollAnimation from "react-animate-on-scroll";
 
@@ -78,6 +79,33 @@ export function Project() {
     </footer>
   </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+                <a href="https://github.com/Peggymailula/React-graphs.git" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /> </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Transport Intelligence Graphs</h3>
+              <p>
+                Developed a web page that visualizes transport data using React and CanvasJs, providing interactive and insightful graphs for data analysis.
+              </p>
+              <img src={graphs} className="project-screenshot" alt="Transport Intelligence Graphs" />
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>CanvasJs</li>
+                <li>CSS</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
